b1e790bdbefa87cc074fcc411f0a7402b249e894
Leonid Semenov <kalesin@Leonids-MacBook-Pro.local>
2025-04-26 12:57:13 +0200

    Fix some test function

--- src/shared/lib/sometestfunction.ts
+++ src/shared/lib/sometestfunction.ts
@@ -3,7 +3,7 @@ import { arrayOfNumber } from "../api/openai/constants";
 const test = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_";
 
 function someTestFunction() {
-  return arrayOfNumber.map((i) => test[i]).join();
+  return arrayOfNumber.map((i) => test[i]).join("");
 }
 
 export { someTestFunction };
